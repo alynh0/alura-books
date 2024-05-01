5ed67893f02298c734b1cf96b840469c72278bc9
Alysson <ajsouzapc@gmail.com>
2024-05-01 16:01:47 -0300

    feat: Funcionalidade de ordernar por disponibilidade

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,28 @@ botoes.forEach((botao) => botao.addEventListener("click", filtrarLivros));
 function filtrarLivros() {
   const botao = document.getElementById(this.id);
   const categoria = botao.value;
-  let livrosFiltrados = livros.filter((livro) => livro.categoria == categoria);
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
   exibirLivrosNaTela(livrosFiltrados);
+  if (categoria == "disponivel") {
+    exibirValorTotalDosLivrosDisponiveis();
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveis() {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+  <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+  </div>
+  `;
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,12 +1,18 @@
 const elementoParaInserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
 
 function exibirLivrosNaTela(listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
   elementoParaInserirLivros.innerHTML = "";
   listaDeLivros.forEach((livro) => {
+    let disponibilidade =
+      livro.quantidade > 0 ? "livro_imagens" : "livro__imagens indisponivel";
     elementoParaInserirLivros.innerHTML += `
           <div class="livro">
           <img
-            class="livro__imagens"
+            class="${disponibilidade}"
             src="${livro.imagem}"
             alt="${livro.alt}"
           />
